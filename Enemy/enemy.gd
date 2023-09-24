5b8308b481e3bbc86908579c0c96b791f18b9b7d
Your Name <you@example.com>
2023-09-24 17:44:24 +0200

    fwefew

---
+++ Enemy/enemy.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+var _velocity = Vector2.ZERO
+
+export var path_to_player = NodePath()
+onready var _agent: NavigationAgent2D = $NavigationAgent2D
+onready var _player = get_node(path_to_player)
+onready var _timer: Timer = $Timer
+
+func _ready() -> void: 
+	_update_pathfinding()
+	_timer.connect("timeout", self, "_update_pathfinding")
+
+func _physics_process(delta: float) -> void:
+	if _agent.is_navigation_finished():
+		return
+	
+	var direction = global_position.direction_to(_agent.get_next_location())
+	
+	var desired_velocity = direction * 100.0
+	var steering = (desired_velocity - _velocity) * delta * 4.0
+	_velocity += steering
+	
+	_velocity = move_and_slide(_velocity)
+
+func _update_pathfinding() -> void:
+	_agent.set_target_location(_player.global_position)
---
+++ Player/player.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+
+export (PackedScene) var Bullet
+
+var SPEED = 120
+
+var DISTANCE_IN_FRONT = 15
+
+onready var light = $Light2D
+onready var maskedLight = $Light2D3
+
+onready var shootPosition = $WeaponStartPositions/ShootPosition
+
+
+# Called when the node enters the scene tree for the first time.
+func _physics_process(delta):
+	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	var move_direction = input_vector.normalized()
+	move_and_slide(SPEED * move_direction)
+	
+	light.rotation = get_angle_to(get_global_mouse_position())
+	maskedLight.rotation = get_angle_to(get_global_mouse_position())
+	
+	var player_direction = (get_global_mouse_position() - position).normalized()
+	var target_position = global_position + player_direction * DISTANCE_IN_FRONT
+	shootPosition.global_position = target_position
+
+func _unhandled_input(event):
+	if event.is_action_released("shoot"):
+		shoot()
+
+func shoot():
+	var bullet_instance = Bullet.instance()
+	add_child(bullet_instance)
+	bullet_instance.global_position = shootPosition.global_position
+	var target = get_global_mouse_position()
+	var direction_to_mouse = bullet_instance.global_position.direction_to(target).normalized()
+	bullet_instance.set_direction(direction_to_mouse)
+
+func _on_hurtBox_area_entered(area):
+	print("area")
+	pass # Replace with function body.
